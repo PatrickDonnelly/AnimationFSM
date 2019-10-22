17412b4fa0f372f16a449ca10fbc2aa6a42fc6bf
PatrickDonnelly <c00236160@itcarlow.ie>
2019-10-22 12:44:07 +0100

    Added cpps and headers for other states

---
+++ AnimationFSM/Falling.h
@@ -0,0 +1,19 @@
+//
+//#ifndef FALLING_H
+//#define FALLING_H
+//
+//#include <State.h>
+//
+//
+//class Falling:public State
+//{
+//	public:
+//		Climbing() {};
+//		~Climbing() {};
+//		void idle(PlayerFSM* a);
+//		void jumping(PlayerFSM* a);
+//	};
+//
+//
+//};
+
---
+++ AnimationFSM/Walking.cpp
@@ -0,0 +1,1 @@
+#include "Walking.h"
